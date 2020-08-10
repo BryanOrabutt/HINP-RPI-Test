b8238bfffe0b80b22991524a3ec181981b5da31e
Bryan Orabutt <borabutt@wustl.edu>
2020-08-09 21:59:34 -0500

    Add HINP files

---
+++ src/C/rpi_gpio/hinp_rpi.c
@@ -0,0 +1,369 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "hinp_rpi.h"
+#include <sched.h>
+
+//Pin accessors -- Use the Broadcom GPIO number *not* the phyiscal pin header number
+//const unsigned char DATA[8] = {5,6,13,19,26,16,20,21}; //data bus -- used to load
+const unsigned char DATA[8] = {7,8,25,24,23,18,15,14}; //data bus -- used to load configuration registers
+const unsigned char MODE[4] = {7,8,25,24}; //address bus -- just an alias of the upper nibble of the data bus
+const unsigned char ADDR[4] = {23,18,15,14}; //mode bus -- just an alias of the lower nibble of the data bus
+//const unsigned char NEG_POL = 7; //negative polarity enable -- POS,NEG = 0,1
+const unsigned char WRITE = 12; //bidirectional bus control -- IN,OUT = 1,0
+const unsigned char STB = 2; //data/address strobe -- addr/mode latched on rising edge, data latched on falling edge
+const unsigned char GEN = 20; //global enable -- OUTPUT,NO_OUTPUT = 1,0
+const unsigned char EXT_AGND = 26; //external AGND enable -- INTERNAL,EXTERNAL = 0,1
+const unsigned char RST_L = 16; //low active reset -- ensures one-shot works properly.
+const unsigned char ADC_CONV = 3; //ADC convert signal
+const unsigned char LOW_GAIN_SDO = 27; //Low gain ADC data 
+const unsigned char TVC_SDO = 4; //TVC ADC data 
+const unsigned char HIGH_GAIN_SDO = 9; //High gain ADC data
+const unsigned char ADC_SCK = 11; //ADC data clock for all three ADCs
+const unsigned char ACQ_ALL = 21; //Acquire all
+const unsigned char FORCE_RESET = 13; //Force digital reset and advance priority encoder during readout.
+const unsigned char COMMON_STOP = 19; //Stop all TVCs.
+const unsigned char TAKE_EVENT = 6; //Start readout.
+const unsigned char OR_OUT = 17; //Logical OR of all channel hit registers.
+
+
+int iter; //iterator for loops
+
+void set_write()
+{
+	GPIO_SET = 1 << WRITE;
+}
+
+void set_read()
+{
+	GPIO_CLR = 1 << WRITE;
+}
+
+void set_addr_mode(char addr, char mode)
+{
+	
+	for(iter = 0; iter < MODE_BITS; iter++)
+	{
+		if(addr & (1 << iter))
+		{
+			GPIO_SET = 1 << ADDR[iter];
+		}
+		else
+		{
+			GPIO_CLR = 1 << ADDR[iter];
+		}
+
+		if(mode & (1 << iter))
+		{
+			GPIO_SET = 1 << MODE[iter];
+		}
+		else
+		{
+			GPIO_CLR = 1 << MODE[iter];
+		}	
+	}
+	
+}
+
+void set_data(char data)
+{
+	
+	for(iter = 0; iter < DATA_BITS; iter++)
+	{
+		if(data & (1 << iter))
+		{
+			GPIO_SET = 1 << DATA[iter];
+		}
+		else
+		{
+			GPIO_CLR = 1 << DATA[iter];
+		}
+	}
+}
+
+void strobe_high()
+{
+	GPIO_SET = 1 << STB;
+}
+
+void strobe_low()
+{
+	GPIO_CLR = 1 << STB;
+}
+
+void set_internal_agnd(char val)
+{
+	if(val == ENABLE)
+	{
+		GPIO_CLR = 1 << EXT_AGND;
+	}
+	else
+	{
+		GPIO_SET = 1 << EXT_AGND;
+	}
+}
+
+void pulse_rst_l()
+{
+	GPIO_CLR = 1 << RST_L;
+	delay_ns(5000);
+	GPIO_SET = 1 << RST_L;
+}
+
+void set_gen(char val)
+{
+	if(val)
+	{
+		GPIO_SET = 1 << GEN;
+	}
+	else
+	{
+		GPIO_CLR = 1 << GEN;
+	}
+}
+
+void delay_ns(long ns)
+{
+	struct timespec t, rt;
+	
+	clock_gettime(CLOCK_REALTIME, &rt);
+	rt.tv_nsec = 0;
+	clock_settime(CLOCK_REALTIME, &rt);
+	clock_gettime(CLOCK_REALTIME, &rt);
+	rt.tv_nsec += ns;
+
+	while(1)
+	{
+		clock_gettime(CLOCK_REALTIME, &t);
+		if(rt.tv_nsec > t.tv_nsec)
+			break;
+	}
+}
+
+void rpi_setup_io()
+{
+   // open /dev/gpiomem 
+   if ((mem_fd = open("/dev/gpiomem", O_RDWR|O_SYNC) ) < 0) 
+   {
+      printf("can't open /dev/gpiomem \n");
+      exit(-1);
+   }
+
+   //mmap GPIO 
+   gpio_map = mmap(
+      NULL,             //Any adddress in our space will do
+      BLOCK_SIZE,       //Map length
+      PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
+      MAP_SHARED,       //Shared with other processes
+      mem_fd,           //File to map
+      GPIO_BASE         //Offset to GPIO peripheral
+   );
+
+   close(mem_fd); //No need to keep mem_fd open after mmap
+
+   if (gpio_map == MAP_FAILED) 
+   {
+      printf("mmap error %d\n", (int)gpio_map);//errno also set!
+      exit(-1);
+   }
+
+   //Point access pointer at mapped region.
+   gpio = (volatile unsigned *)gpio_map;
+}
+
+//setup GPIO modes and set initial control line states.
+void rpi_configure()
+{
+    //Must use INP_GPIO before OUT_GPIO. INP_GPIO will ensure the two MSBs for FSEL are 0
+    //OUT_GPIO will set LSB of FSEL to 1 so that the function select is 001 which is output
+    INP_GPIO(WRITE);
+    OUT_GPIO(WRITE);
+	GPIO_CLR = 1 << WRITE;
+    
+	INP_GPIO(STB);
+    OUT_GPIO(STB);
+	GPIO_CLR = 1 << STB;
+	delay_ns(1000);
+	GPIO_SET = 1 << STB;
+    
+    INP_GPIO(GEN);
+    OUT_GPIO(GEN);
+	GPIO_CLR = 1 << GEN;
+	delay_ns(1000);
+	GPIO_SET = 1 << GEN;
+    
+    INP_GPIO(EXT_AGND);
+    OUT_GPIO(EXT_AGND);
+	GPIO_CLR = 1 << EXT_AGND;
+	delay_ns(1000);
+	GPIO_SET = 1 << EXT_AGND;
+    
+    INP_GPIO(RST_L);
+    OUT_GPIO(RST_L);
+	GPIO_SET = 1 << RST_L;   
+    
+    INP_GPIO(ADC_CONV);
+    OUT_GPIO(ADC_CONV);
+    GPIO_CLR = 1 << ADC_CONV;
+    
+    INP_GPIO(ADC_SCK);
+    OUT_GPIO(ADC_SCK);
+    GPIO_SET = 1 << ADC_SCK; 
+    
+    INP_GPIO(LOW_GAIN_SDO);
+    
+    INP_GPIO(HIGH_GAIN_SDO);
+    
+    INP_GPIO(TVC_SDO);
+    
+    INP_GPIO(ACQ_ALL);
+    OUT_GPIO(ACQ_ALL);
+    GPIO_CLR = 1 << ACQ_ALL;
+    
+    INP_GPIO(FORCE_RESET);
+    OUT_GPIO(FORCE_RESET);
+    GPIO_CLR = 1 << ACQ_ALL;
+    
+    INP_GPIO(COMMON_STOP);
+    OUT_GPIO(COMMON_STOP);
+    GPIO_CLR = 1 << COMMON_STOP;
+    
+    INP_GPIO(TAKE_EVENT);
+    OUT_GPIO(TAKE_EVENT);
+    GPIO_CLR = 1 << TAKE_EVENT;
+    
+    INP_GPIO(OR_OUT);      
+ 
+    for(iter = 0; iter < 8; iter++)
+    {
+        INP_GPIO(DATA[iter]);
+        OUT_GPIO(DATA[iter]);
+		GPIO_CLR = 1 << DATA[iter];
+    }
+
+    GPIO_SET = 1 << WRITE; //write a 1 to the GPIOSET1 register bit 5
+    GPIO_CLR = 1 << RST_L; //bring RST low to force oneshot state low
+	usleep(1000); //hold RST low for 1 ms
+    GPIO_SET = 1 << RST_L; //bring RST high to enable oneshot
+}
+
+void rpi_cleanup_gpio()
+{
+	for(iter = 0; iter < 32; iter++)
+	{
+		GPIO_CLR = 1 << iter;
+		INP_GPIO(iter);
+	}
+}
+
+
+void set_conv(char state)
+{
+    
+	if(state)
+	{
+		GPIO_SET = 1 << ADC_CONV;
+	}
+	else
+	{
+		GPIO_CLR = 1 << ADC_CONV;
+	}
+}
+
+void set_acq_all(char state)
+{
+	if(state)
+	{
+		GPIO_SET = 1 << ACQ_ALL;
+	}
+	else
+	{
+		GPIO_CLR = 1 << ACQ_ALL;
+	}
+}
+
+void set_common_stop(char state)
+{
+	if(state)
+	{
+		GPIO_SET = 1 << COMMON_STOP;
+	}
+	else
+	{
+		GPIO_CLR = 1 << COMMON_STOP;
+	}
+}
+
+void force_reset_low(void)
+{
+    GPIO_CLR = 1 << FORCE_RESET;
+}
+
+void force_reset_high(void)
+{
+    GPIO_SET = 1 << FORCE_RESET;
+}
+
+void set_take_event(char state)
+{
+	if(state)
+	{
+		GPIO_SET = 1 << TAKE_EVENT;
+	}
+	else
+	{
+		GPIO_CLR = 1 << TAKE_EVENT;
+	}
+}
+
+
+struct adc_readings read_adcs(void)
+{
+    struct adc_readings readings = {0};
+    set_conv(1);
+    delay_ns(CONV_TIME_NS);
+    set_conv(0);
+    delay_ns(CONV_SETUP_NS);
+    for(iter = 0; iter < 16; iter++)
+    {
+        GPIO_CLR = 1 << ADC_SCK;
+        delay_ns(SCK_PERIOD_NS/2);
+        GPIO_SET = 1 << ADC_SCK;
+        //delay(ns(SCK_PERIOD_NS/2);
+        readings.tvc |= (GET_GPIO(TVC_SDO)) ? (1 << iter):0;
+        readings.low_gain |= (GET_GPIO(LOW_GAIN_SDO)) ? (1 << iter):0;
+        readings.high_gain |= (GET_GPIO(HIGH_GAIN_SDO)) ? (1 << iter):0;
+    }
+    
+    return readings;
+}
+
+void init_adc(void)
+{
+    set_conv(1);
+    delay_ns(CONV_TIME_NS);
+    set_conv(0);
+    delay_ns(CONV_SETUP_NS);
+    for(iter = 0; iter < 16; iter++)
+    {
+        GPIO_CLR = 1 << ADC_SCK;
+        delay_ns(SCK_PERIOD_NS/2);
+        GPIO_SET = 1 << ADC_SCK;
+        delay_ns(SCK_PERIOD_NS/2);
+    }
+}
+
+char read_channel_number(void)
+{
+    char channel = 0;
+    for(iter = 0; iter < ADDR_BITS; iter++)
+    {
+        channel |= GET_GPIO(ADDR[iter]) ? (1 << iter):0;
+    }
+    
+    return channel;
+}
---
+++ src/C/rpi_gpio/hinp_rpi.h
@@ -0,0 +1,140 @@
+#define RPI4
+
+#ifdef RPI4
+    #define BCM2708_PERI_BASE        0xFE000000 //Start of peripheral memory mappings
+#else
+    #define BCM2708_PERI_BASE        0x3F000000 //Start of peripheral memory mappings
+#endif
+
+#define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) //Start of GPIO control regs
+
+#define PAGE_SIZE (4*1024)
+#define BLOCK_SIZE (4*1024)
+
+int  mem_fd;
+void *gpio_map;
+
+//I/O access pointer
+volatile unsigned *gpio;
+
+
+//GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
+#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
+#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
+
+#define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
+#define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
+
+#define GET_GPIO(g) (*(gpio+13)&(1<<g)) // 0 if LOW, (1<<g) if HIGH
+
+#define GPIO_PULL *(gpio+37) // Pull up/pull down
+#define GPIO_PULLCLK0 *(gpio+38) // Pull up/pull down clock
+
+#define CONV_TIME_NS    3200
+#define CONV_SETUP_NS   60
+#define SCK_PERIOD_NS   50
+
+/* Number of channles */
+#define CHANNELS 16
+
+/* Register modes */
+#define NOWLIN_MODE		0b00000000
+#define AGND_MODE		0b00000001
+#define ONESHOT_MODE	0b00000001
+#define LOCKOUT_MODE	0b00000101
+#define DAC_MODE		0b00000110
+#define GMODE			0b00001000
+
+/* Channel Addresses */
+#define CH0		0b00000000
+#define CH1		0b00010000
+#define CH2		0b00100000
+#define CH3		0b00110000
+#define CH4		0b01000000
+#define CH5		0b01010000
+#define CH6		0b01100000
+#define CH7		0b01110000
+#define CH8		0b10000000
+#define CH9		0b10010000
+#define CH10	0b10100000
+#define CH11	0b10110000
+#define CH12	0b11000000
+#define CH13	0b11010000
+#define	CH14	0b11100000
+#define CH15	0b11110000
+#define COMMON	0b00000000
+
+/* Bit widths */
+#define DATA_BITS	8
+#define MODE_BITS	4
+#define ADDR_BITS	4
+
+/* GUI settings below this line */
+
+/* AGND settings */
+#define AGND_1_36		0
+#define AGND_1_43		1
+#define AGND_1_49		2
+#define AGND_1_56		3
+#define	AGND_1_63		4
+#define	AGND_1_69		5
+#define AGND_1_77		6
+#define AGND_1_84		7
+#define AGND_NOMINAL	4
+#define AGND_MAX		7
+#define AGND_MIN		0
+
+/* Digital auto reset modes */
+#define AR_DIGITAL_100      0
+#define AR_DIGITAL_1000		1
+
+/* ADC Buffer Modes */
+#define BUFF_NEG_50     0
+#define BUFF_NEG_25     1
+#define BUFF_25         3
+#define BUFF_50         4
+
+/* TVC Modes */
+#define TVC_2US     1
+#define TVC_250NS   0
+
+/* Nowlin Modes */
+#define NOWLIN_SHORT	0
+#define NOWLIN_LONG		1
+
+#define COLLECT_HOLES       1
+#define COLLECT_ELECTRONS   0
+
+#define ENABLE	0
+#define DISABLE	1
+
+struct adc_readings
+{
+    short tvc;
+    short low_gain;
+    short high_gain;
+};
+
+void rpi_cleanup_gpio(void);
+void rpi_configure(void);
+void rpi_setup_io(void);
+void delay_ns(long ns);
+void set_addr_mode(char addr, char mode);
+void set_data(char data);
+void strobe_high(void);
+void strobe_low(void);
+void set_internal_agnd(char val);
+void pulse_rst_l(void);
+void set_gen(char val);
+void set_write(void);
+void set_read(void);
+void set_conv(char state);
+void set_acq_all(char state);
+void set_common_stop(char state);
+void force_reset_low(void);
+void force_reset_high(void);
+void set_take_event(char state);
+struct adc_readings read_adcs(void);
+void init_adcs(void);
+char read_channel_number(void);
